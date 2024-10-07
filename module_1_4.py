6954060912a6ccf6939d62b700bacd17a380e09c
zhenya-mom <zhen.lemesh@icloud.com>
2024-10-07 17:25:29 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+homework = 12
+time_hour = 1.5
+course = "Python"
+time_one_homework = time_hour/homework
+print ("Курс:", course+ "," +  " всего задач: "+str(homework)+","" затрачено часов: "+str(time_hour)+","  +" среднее время выполнения: "  +str (time_one_homework)+ " часа.")
+
+
+
